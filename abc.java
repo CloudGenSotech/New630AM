18b5b7d351a3f1a993487f526e2f26f7bc6b1793
ThotaThanish <sriurr@gmail.com>
2017-10-31 07:08:17 +0530

    testbranch

---
+++ abc.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class abc
+{
+public static void main(String[] args)
+{
+System.out.println("HELLO JAVA HOW R U");
+int a,b,c;
+a=50;
+b=300;
+c=123;
+c=a+b+c;
+System.out.println("THe sum of Total Values are "+c);
+}
+}
