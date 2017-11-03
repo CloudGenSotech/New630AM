8b1ede4b0680bbe884dac6d218fe96a20d3c8dd3
ThotaThanish <sriurr@gmail.com>
2017-11-03 06:53:14 +0530

    cprogram

---
+++ HELLO123.c
@@ -0,0 +1,14 @@
+#include<STDIO.H>
+#include<conio.h>
+
+
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n\n Etner ab values");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("The sum of two values are %d",c);
+getch();
+}
