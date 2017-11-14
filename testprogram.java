07d54460f7535a346234e83b3ab422e4cb8b6294
ThotaThanish <sriurr@gmail.com>
2017-11-14 19:53:13 +0530

    ddd

---
+++ testprogram.java
@@ -0,0 +1,15 @@
+class testprogram
+{
+	public static void main(String[] args) 
+		{
+		int a,b,c,d;
+		a=600;
+		c=400;
+		b=a+c;
+		d=a-b;
+		System.out.println("The a value is "+b);
+		System.out.println("The d value is "+d);
+				System.out.println("Hello World!");
+					}
+					}
+
